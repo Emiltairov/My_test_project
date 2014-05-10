6616a3685244512487d796c48cf1add71795842f
123 <123>
2014-05-10 14:41:32 +0400

    'upd_routes

--- vendor/extensions/feedbacks/config/routes.rb
+++ vendor/extensions/feedbacks/config/routes.rb
@@ -3,34 +3,17 @@ Refinery::Core::Engine.routes.draw do
   # Frontend routes
   namespace :feedbacks do
     resources :feedbacks, :path => '', :only => [:index, :new, :create]
+
   end
 
   # Admin routes
   namespace :feedbacks, :path => '' do
     namespace :admin, :path => Refinery::Core.backend_route do
-      resources :feedbacks do
+      resources :feedbacks, :settings do
         collection do
           post :update_positions
         end
       end
     end
   end
-
-
-  # Frontend routes
-  namespace :feedbacks do
-    resources :settings, :only => [:index, :show]
-  end
-
-  # Admin routes
-  namespace :feedbacks, :path => '' do
-    namespace :admin, :path => "#{Refinery::Core.backend_route}/feedbacks" do
-      resources :settings do
-        collection do
-          post :update_positions
-        end
-      end
-    end
-  end
-
 end
